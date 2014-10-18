bf9f85bcf4535fc6b0a76fe9cc8fdadb82422bd1
letcheng <letcheng@tcxiaoyi.com>
2014-10-18 15:13:07 +0800

    fisrt commit

---
+++ LinearLayout和RelativeLayout/src/com/tcxy/dial/MainActivity.java
@@ -0,0 +1,16 @@
+package com.tcxy.dial;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity{
+
+
+	protected void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.login_combine);
+	}
+	
+}
